173fd8c6390260a02f33b6e7ce654380b69429ff
byhugom6 <byhugom6@gmail.com>
2017-08-21 12:28:07 +0200

    first commit

---
+++ CryptoNoteWallet.cmake
@@ -0,0 +1,4 @@
+
+set(CN_PROJECT_NAME "mercoin")
+set(CN_CURRENCY_DISPLAY_NAME "MerCoin")
+set(CN_CURRENCY_TICKER "MRN")
---
+++ build/CryptoNoteWalletConfig.h
@@ -0,0 +1,9 @@
+
+#pragma once
+
+namespace WalletGui {
+
+const char WALLET_CURRENCY_DISPLAY_NAME[] = "MerCoin";
+const char WALLET_CURRENCY_TICKER[] = "MRN";
+
+}
